c16ae9923fe888f84e6a0fea1162a0276431d470
Victor Hugo Feracin <victorhugo.feracin@gmail.com>
2023-10-21 11:44:44 -0300

    Finishing CidadesHTMLHandler method

--- es46a-2023-2/01-design-patterns-template-strategy/src/LoadChain/CidadesHTMLHandler.js
+++ es46a-2023-2/01-design-patterns-template-strategy/src/LoadChain/CidadesHTMLHandler.js
@@ -4,10 +4,18 @@ class CidadesHTMLHandler extends CidadesHandler {
 
   handleRequest(request, format) {
     if (format.toLowerCase() === 'html') {
-      console.log(super.getCidades());
-      return "manipulando html";
+      const bufferToStr = super.getCidades().toString(); // Transforma o arquivo lido para String
+      const regex = /<li>(.*?)<\/li>/g; // Regex para obter tudo que está dentro da lista no HTML
+      const formattedCities = [];
+      let i;
+
+      while((i = regex.exec(bufferToStr)) !== null) { //Continua buscando na String até não ter mais nenhum item na lista
+        formattedCities.push(i[1]);
+      }
+
+      return formattedCities;
     } else {
-      return super.handleRequest(request, format); // Pass the 'format' argument to the next handler
+      return super.handleRequest(request, format); // Caso o formato não seja o especificado para este método, o handler trata
     }
   }
 }
