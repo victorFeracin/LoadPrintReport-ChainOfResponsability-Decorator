c91627d727845632d05aaf94f4d7e5ee5f2294ff
Victor Hugo Feracin <victorhugo.feracin@gmail.com>
2023-10-21 14:50:24 -0300

    Finishing CidadesXMLHandler methods and deleting unnecessary files

--- es46a-2023-2/01-design-patterns-template-strategy/index.js
+++ es46a-2023-2/01-design-patterns-template-strategy/index.js
@@ -4,4 +4,3 @@ const [cmd, filename, formatLoad, formatPrint] = process.argv;
 
 let loader = new Client(); //Instancia de client
 const arrayCitiesList = loader.loadFile(formatLoad); //Essa const salva o array contendo as cidades
-console.log(arrayCitiesList); //Mostra o array (Apagar depois)
--- es46a-2023-2/01-design-patterns-template-strategy/src/LoadChain/CidadesXMLHandler.js
+++ es46a-2023-2/01-design-patterns-template-strategy/src/LoadChain/CidadesXMLHandler.js
@@ -3,9 +3,18 @@ import CidadesHandler from "./CidadesHandler.js";
 export default class CidadesXMLHandler extends CidadesHandler {
   handleRequest(format) {
     if(format.toLowerCase() === 'xml') {
-      return "manipulando xml";
+      this.ler('./data/cidades.xml');
+      const regex = /<cidades>(.*?)<\/cidades>/g; //Expressao para obter tudo que está dentro da lista no XML
+      const formattedCities = [];
+      let i;
+
+      while((i = regex.exec(this.getCidadesString())) !== null) { //Continua buscando na String até não ter mais nenhum item na lista
+        formattedCities.push(i[1]); //i[0] contém a linha completa (<cidades>Exemplo</cidades>), enquanto que i[1] traz somente o que está dentro das tags
+      }
+
+      return formattedCities; //Retorna o array formatado
     } else {
-      return super.handleRequest(format);
+      return super.handleRequest(format); //Caso o formato não seja o especificado para este método, o handler trata
     }
   }
 }
